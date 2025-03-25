dfcb61992c3e0b1348e11551b3a8f091e24c7d00
KhonChhau <chhay@assurasolutions.com.kh>
2025-03-24 19:08:01 -0700

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-center">
+        <h1 className='text-danger bg-black'>test </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
